6216697f220d54594b648812156c81d02b6533ca
Pavel Bakanov <pashtetrus@gmail.com>
2024-06-26 17:52:46 -0400

    init commit

---
+++ src/test/java/ru/skillbox/student_registration/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox.student_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
